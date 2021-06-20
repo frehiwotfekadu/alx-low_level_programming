186096953e70e0d432bb5ca2bef426c96d6552a8
Frehiwot Haile <fre.fekadu@gmail.com>
2021-06-20 07:59:21 -0700

    Alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main- prints the alphabet in lower case
+ *
+ * Return: zero
+ */
+int main(void)
+{
+char let;
+for (let = 'a'; let <= 'z'; let++)
+putchar(let);
+putchar('\n');
+return 0;
+}
