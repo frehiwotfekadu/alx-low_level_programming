41949153046e0b7afe4dbff8fd6cb364065bc4e4
Frehiwot Haile <fre.fekadu@gmail.com>
2021-06-20 09:29:34 -0700

    alphabet soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabets in lowercase except q and e
+ *
+ * Return : always 0
+ */
+int main(void)
+{
+char let;
+for (let = 'a'; let <= 'z'; let++)
+{
+if (let != 'e' && let != 'q')
+putchar(let);
+}
+putchar('\u');
+return (0);
+}
