3054cca774ea35356c1be2565a3d3215c5ad51e5
Frehiwot Haile <fre.fekadu@gmail.com>
2021-06-20 06:14:48 -0700

    Positive anything

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,16 +10,16 @@ include <stdlib.h>
 int main(void)
 {
 int n;
-
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n < 0)
 {
 printf("%d is negative\n", n);
 }
-else if (n 
+ else if (n > 0) 
 {
-printf("%d is positive\n", n);}
+printf("%d is positive\n", n);
+}
 else
 {
 printf("0 is zero\n");
