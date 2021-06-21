15ef9a28729398ef54ddeb83fad266354a4ca187
Frehiwot Haile <fre.fekadu@gmail.com>
2021-06-20 18:11:57 -0700

    Patience

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of single-digit numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int i;
+for (i = '0'; i <= '9'; i++)
+{
+putchar(i);
+if (i == '9')
+break;
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+} 
