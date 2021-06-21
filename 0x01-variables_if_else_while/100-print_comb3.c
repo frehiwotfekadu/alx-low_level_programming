3c46859237fb31ad1918c3690b5ca70ffe68cbfa
Frehiwot Haile <fre.fekadu@gmail.com>
2021-06-20 18:57:00 -0700

    Inventing

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,19 +6,15 @@
  */
 int main(void)
 {
-int n;
 int i;
-for (n = 0; n < 10; n++)
+for (i = 0; i <= 99; i++)
 {
-for (i = 0; i < 10; i++)
-{
-putchar((n % 10) + '0');
+putchar((i / 10) + '0');
 putchar((i % 10) + '0');
-if (n == 9 && i == 9)
-continue;	  
+if (i == 99)
+break;
 putchar(',');
 putchar(' ');
-}
 }
 putchar('\n');
 return (0);
