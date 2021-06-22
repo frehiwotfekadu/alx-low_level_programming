b0ff29611f2a16ff17b84b9f4a1dcb2aa662d128
Frehiwot Haile <fre.fekadu@gmail.com>
2021-06-22 13:07:25 -0700

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: integer from which to get an absolute value
+ * Return: Void
+ */
+int _abs(int n)
+{
+if (n < 0)
+{
+return (n * -1);
+}
+return (n);
+}
