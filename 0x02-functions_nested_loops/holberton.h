9d4dddc8d17e2a65c44867219e11cd0e11c90c12
Frehiwot Haile <fre.fekadu@gmail.com>
2021-06-22 11:37:57 -0700

    custom header

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,23 +1,15 @@
-int _putchar(char c);
-/*Putchar holberton function*/
-void print_alphabet(void);
-/*Print alphabet lowercase*/
-void print_alphabet_x10(void);
-/*Print alphabet 10 times*/
-int _islower(int c);
-/*Checks for lowercase character*/
-int _isalpha(int c);
-/*Checks for alphabet character*/
-int print_sign(int n);
-/*Give the sign of a number*/
-int _abs(int);
-/*Computes the absolute value of an integer*/
-int print_last_digit(int);
-/*Function to get the last digit of any number*/
-void jack_bauer(void);
-/*Function to count every minute until 24 minutes*/
-void times_table(void);
-/*Function to print 9 times table*/
-void print_to_98(int n);
-/*Function to print numbers until 98*/
-int add(int, int);
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+extern int _putchar(char c);
+extern void jack_bauer(void);
+extern void print_alphabet(void);
+extern void print_alphabet_x10(void);
+extern int _islower(int c);
+extern int _isalpha(int c);
+extern int print_sign(int n);
+extern int _abs(int);
+extern int print_last_digit(int);
+extern void times_table(void);
+extern int add(int, int);
+extern void print_to_98(int n);
+#endif
