9383ed2fe121d902726c1b60fb2647cce86ed467
Frehiwot Haile <fre.fekadu@gmail.com>
2021-06-22 10:48:57 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * int _islower(int c) - a function that checks for lowercase character.
+ *
+ * Return: 1 if c is lowercase, otherwise returns 0
+ */
+int_islower(int c)
+{
+int lowercase;
+if (c >= 97 && c <= 122)
+{
+lowercase = 1;
+}
+else
+{
+lowercase = 0;
+}
+return (lowercase);
+}
