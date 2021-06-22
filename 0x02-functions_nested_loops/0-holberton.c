a7c95ff974785670d2c4ec9d0807456a735f7c35
Frehiwot Haile <fre.fekadu@gmail.com>
2021-06-22 01:36:03 -0700

    0._putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - prints Holberton
+ *
+ * Return: Always 0
+ */
+int main()
+{
+int i;
+char msg[] = "Holberton";
+for (i = 0; i < strlen(msg); i++)
+{
+putchar(msg[i]);
+}
+putchar('\n');
+return(0);
+}
