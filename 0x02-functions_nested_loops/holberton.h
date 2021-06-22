de3f6c974a614f49087e8e373ddef9438742e3c0
Frehiwot Haile <fre.fekadu@gmail.com>
2021-06-22 08:53:25 -0700

    custom header

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,23 @@
+int _putchar(char c);
+/*Putchar holberton function*/
+void print_alphabet(void);
+/*Print alphabet lowercase*/
+void print_alphabet_x10(void);
+/*Print alphabet 10 times*/
+int _islower(int c);
+/*Checks for lowercase character*/
+int _isalpha(int c);
+/*Checks for alphabet character*/
+int print_sign(int n);
+/*Give the sign of a number*/
+int _abs(int);
+/*Computes the absolute value of an integer*/
+int print_last_digit(int);
+/*Function to get the last digit of any number*/
+void jack_bauer(void);
+/*Function to count every minute until 24 minutes*/
+void times_table(void);
+/*Function to print 9 times table*/
+void print_to_98(int n);
+/*Function to print numbers until 98*/
+int add(int, int);
