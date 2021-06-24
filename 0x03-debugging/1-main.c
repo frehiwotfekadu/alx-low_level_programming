80549831c4ba50e68a9d839225539585f50968f5
Frehiwot Haile <fre.fekadu@gmail.com>
2021-06-24 10:32:12 -0700

    Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,7 +2,6 @@
 
 /**
  * main - causes an infinite loop
- * @\\o: used to comment out unwanted infinite loop
  * Return: 0
  */
 
@@ -13,7 +12,6 @@ int i;
 printf("Infinite loop incoming :(\n");
 
 i = 0;
-
 /*
  * while (i < 10)
  * {
