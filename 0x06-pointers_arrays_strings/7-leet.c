688e8f1a770743369825b5b8fa7ad6957cb97808
Frehiwot Haile <fre.fekadu@gmail.com>
2021-07-01 01:37:00 -0700

    6th project

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * leet - encodes a string in to 1337
+ * @str: string to be encoded
+ * Return: Always 0
+ */
+char *leet(char *str)
+{
+int i = 0, k;
+char s[] = "aAeEoOtTlL";
+char s1[] = "4433007711";
+for (; str[i] != '\0'; i++)
+{
+for (k = 0; k <= 9; k++)
+{
+if (s[k] == str[i])
+{
+str[i] = s1[k];
+}
+}
+}
+return (str);
+}
