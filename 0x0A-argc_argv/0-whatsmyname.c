ddf4f523f09c4ecb95bff61c9ce93321f33aaed9
Frehiwot Haile <fre.fekadu@gmail.com>
2021-07-12 12:15:41 -0700

    whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,10 @@
-#include "holberon.h"
+#include "holberton.h"
 #include <stdio.h>
 /**
- * main - prints it's name followed by a new line
+ * main - prints its name, followed by a new line
  * @argc: argument count
  * @argv: argument vector
- * Return: Always 0
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
