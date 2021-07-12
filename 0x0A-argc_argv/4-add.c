aa44cf21c8fc7bbd24f879be6dd2b6bb81db38cf
Frehiwot Haile <fre.fekadu@gmail.com>
2021-07-12 04:34:22 -0700

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - adds all positive numbers and prints it
+ * @argc: arguement count
+ * @argv: argument vector
+ * Return: 0 if no errors, 1 if invalid argument
+ */
+int main(int argc, char *argv[])
+{
+int i, j, res = 0;
+if (argc > 1)
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j]; j++)
+if (argv[i][j] < '0' || argv[i][j] > '9')
+return (printf("Error\n"), 1);
+}
+printf("%i\n", res);
+return (0);
+}
