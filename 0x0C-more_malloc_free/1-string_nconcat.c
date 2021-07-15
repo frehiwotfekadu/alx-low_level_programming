5cbf6c25fc01182ee8361fdeebb45d529bdc3e89
Frehiwot Haile <fre.fekadu@gmail.com>
2021-07-15 12:25:32 -0700

    nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,43 +1,36 @@
-#include "holberton.h"
+include "holberton.h"
+#include <stdlib.h>
 /**
- * string_nconcat - concatenates two strings
- * @s1: destination string
- * @s2: source string
- * @n: number of bytes from s2 to be copied
- * Return: concatenated string
+ * string_nconcat - concatenates n number of s1 to s2
+ *
+ * @s1: first string
+ * @s2: second string
+ * @n: number of s2 to concatenate
+ *
+ * Return: s1_s2
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int i, j, size, len1, len2;
-char *ptr;
+char *s1_s2;
+unsigned int size1, size2;
+unsigned int count, count2;
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-len1 = _strlen(s1);
-len2 = _strlen(s2);
-if (n > len2)
-n = len2;
-size = len1 + n;
-ptr = malloc(sizeof(char) * size + 1);
-if (!ptr)
+for (size1 = 0; s1[size1] != '\0'; size1++)
+;
+for (size2 = 0; s2[size2] != '\0'; size2++)
+;
+if (n < size2)
+size2 = n;
+s1_s2 = malloc(sizeof(char) * (size1 + size2 + 1));
+if (s1_s2 == NULL)
 return (NULL);
-for (i = 0; i < len1; i++)
-ptr[i] = s1[i];
-for (j = 0; j < n; j++, i++)
-ptr[i] = s2[j];
-ptr[i] = 0;
-return (ptr);
-}
-/**
- * _strlen - returns the length of a string
- * @s: string s
- * Return: length of string
- */
-int _strlen(char *s)
-{
-char *p = s;
-while (*s)
-s++;
-return (s - p);
+for (count = 0; count < size1; count++)
+s1_s2[count] = s1[count];
+for (count2 = 0; count2 < size2; count2++, count++)
+s1_s2[count] = s2[count2];
+s1_s2[count] = '\0';
+return (s1_s2);
 }
